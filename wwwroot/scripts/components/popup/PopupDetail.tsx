eef5efe7db06c54a0f33a50f2cc2276fde8a2275
sk <content.type.text.net@gmail.com>
2026-05-29 19:34:46 +0900

    レンダリング範囲分割

--- wwwroot/scripts/components/popup/PopupDetail.tsx
+++ wwwroot/scripts/components/popup/PopupDetail.tsx
@@ -1,38 +1,22 @@
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import { Box, IconButton, LinearProgress, Stack } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import type { FC } from 'react';
 import { usePopupStore } from '../../hooks/usePopupStore';
+import PopupHeader from './PopupHeader';
 import PopupLogItem from './PopupLogItem';
 
 const PopupDetail: FC = () => {
 	const logs = usePopupStore((a) => a.logs);
-	const setting = usePopupStore((a) => a.setting);
-	const clearLogs = usePopupStore((a) => a.clearLogs);
-	const timeoutProgress = usePopupStore((a) => a.timeoutProgress);
 
 	return (
 		<Box
-			sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'stretch',
+				alignItems: 'flex-end',
+			}}
 		>
-			<Stack
-				direction="row"
-				spacing={1}
-				sx={{ justifyContent: 'center', alignItems: 'center' }}
-			>
-				{setting.autoClose && (
-					<LinearProgress
-						sx={{ width: '100px', height: '10px' }}
-						variant="determinate"
-						color="info"
-						min={0}
-						max={1}
-						value={timeoutProgress}
-					/>
-				)}
-				<IconButton onClick={() => clearLogs()}>
-					<HighlightOffIcon />
-				</IconButton>
-			</Stack>
+			<PopupHeader />
 
 			<Stack direction="column" spacing={1} sx={{ mb: 1, maxWidth: '50vw' }}>
 				{logs.map((a) => (
