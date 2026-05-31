4189296a8ec87a97bf0a08bc5199375a98f90cf4
sk <content.type.text.net@gmail.com>
2026-05-31 13:51:20 +0900

    多分忘れるのでコメントだけ追加

--- Elin.Plugin.Main/Models/Settings/FrontendSetting.cs
+++ Elin.Plugin.Main/Models/Settings/FrontendSetting.cs
@@ -24,6 +24,15 @@ namespace Elin.Plugin.Main.Models.Settings
         public virtual string CssFontSize { get; set; } = string.Empty;
         public virtual int ElementLimit { get; set; }
 
+        /// <summary>
+        /// ハイライト設定。
+        /// </summary>
+        /// <remarks>
+        /// <para>この設定値はプラグイン側では使用しない。</para>
+        /// <para>すべてフロントエンド側で処理される。</para>
+        /// <para>フロントエンドで base64 デコードした後に JSON として解釈され、フロントから保存APIを実行する際にも base64 デコードする。</para>
+        /// <para>ユーザーに直接編集させることを禁止することが目的じゃなくて、JSON内の \ が BepInEx で上手いこと動かないっぽいのでその回避が目的。</para>
+        /// </remarks>
         [GeneratePluginConfigDescription(nameof(PluginLocalizationConfig.FrontendSettingHighlightV2), AllLanguage = true)]
         public virtual string HighlightV2 { get; set; } = string.Empty;
 
